217e88ee6e3c63f22c4ece5bd0cc5e7b7d7bca3c
Pratik Kumar <pratik.k2004@gmail.com>
2025-10-28 18:14:48 +0530

    Token Improvement

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # bot.py
+import os
 import random
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
 # Put your Discord bot token here
-TOKEN = "YOUR_DISCORD_TOKEN"
-
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+print("Token loaded:", bool(TOKEN))  # For testing
 # Intents (make sure 'Message Content Intent' is enabled in the Discord Developer Portal)
 intents = discord.Intents.default()
 intents.message_content = True
